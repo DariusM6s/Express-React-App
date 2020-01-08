0d456c84bcfd0b214eb7c587a9d6db31291b60ce
DariusM6s <dariusm6s@outlook.com>
2020-01-08 20:13:40 +0000

    Initial commit before rename old React names

---
+++ src/app/componenets/Main.jsx
@@ -0,0 +1,19 @@
+import { store } from '../store';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { ConnectedDashboard } from './Dashboard';
+import { Router, Route } from 'react-router-dom';
+import { history } from '../store/history';
+import { ConnectedNavigation } from './Navigation';
+
+export const Main = () => (
+	<Router history={history}>
+		<Provider store={store}>
+			<div>
+				{<ConnectedNavigation />}
+				{/* <ConnectedDashboard /> */}
+				<Route exact path='/dashboard' render={() => <ConnectedDashboard />} />
+			</div>
+		</Provider>
+	</Router>
+);
---
+++ src/app/index.jsx
@@ -0,0 +1,7 @@
+import { store } from './store/index';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Main } from './componenets/Main';
+// import { Dashboard } from './componenets/Dashboard';
+
+ReactDOM.render(<Main />, document.getElementById('app'));
